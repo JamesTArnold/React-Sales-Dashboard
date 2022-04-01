1052e7532aa14a87d0cf8c526fa08fe3379540a2
James T. Arnold <jamestarnold85@gmail.com>
2022-04-01 14:19:48 -0500

    created Navbar

--- react-sales-app/src/Nav/Nav.js
+++ react-sales-app/src/Nav/Nav.js
@@ -1,12 +1,45 @@
-import './Nav.scss';
+import logo from '../logo.svg';
+import "./Nav.scss";
 
 function Nav() {
-    return (
-      <div className="App">
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark  bg-primary"  >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+          <img className="navbar-brand-icon" src={logo} alt="" width="38" height="34" ></img>
+            React Sales
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Performance
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Opportunities
+                </a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
 
-      </div>
-    );
-  }
-  
-  export default Nav;
-  
+export default Nav;
